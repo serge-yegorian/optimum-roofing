d56f60353307cc98c2378708e655907bc722dd28
SEERRGEE <ieg.sergo@gmail.com>
2023-07-18 20:42:46 -0400

    add alt tags to images

--- src/Benefits/Benefits.js
+++ src/Benefits/Benefits.js
@@ -8,17 +8,17 @@ export default function Benefits() {
         <div className="benefits__container">
             <h3 className="benefits__reason">Reason #1</h3>
             <p className='benefits__description'>Paragraph that explains more scecifically why...Paragraph that explains more scecifically why...Paragraph that explains more scecifically why...</p>
-            <img className="benefits__icon" src={require('../square.jpeg')}/>
+            <img alt="icon of a benefit" className="benefits__icon" src={require('../square.jpeg')}/>
         </div>
         <div className="benefits__container">
             <h3 className="benefits__reason">Reason #2</h3>
             <p className='benefits__description'>Paragraph that explains more scecifically why...Paragraph that explains more scecifically why...Paragraph that explains more scecifically why...</p>
-            <img className="benefits__icon" src={require('../square.jpeg')}/>
+            <img alt="icon of a benefit" className="benefits__icon" src={require('../square.jpeg')}/>
         </div>
         <div className="benefits__container">
             <h3 className="benefits__reason">Reason #3</h3>
             <p className='benefits__description'>Paragraph that explains more scecifically why...Paragraph that explains more scecifically why...Paragraph that explains more scecifically why...</p>
-            <img className="benefits__icon" src={require('../square.jpeg')}/>
+            <img alt="icon of a benefit" className="benefits__icon" src={require('../square.jpeg')}/>
         </div>
         </div>
     </section>
